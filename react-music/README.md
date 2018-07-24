09323dada55a669dab7a218c331d6675e9a68a63
tanglin <2196184435@qq.com>
2018-07-24 17:53:14 +0800

    react-QQ-music

---
+++ react-music/README.md
@@ -0,0 +1,19 @@
+# React Music
+
+## 使用 rewire 定制 webpack 工作流
+
+- 安装 rewire
+`$ yarn add rewire proxyquire`
+
+- 新建 scripts 目录
+
+- 更改 npm script
+
+
+1. stylus
+2. alias
+3. autoprefixer
+
+## react 路由配置
+`import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'`
+Switch : router-view
---
+++ react-music/src/components/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import Recommend from './recommend/Recommend'
+import Ranking from './ranking/Ranking'
+import Search from './search/Search'
+
+import { BrowserRouter as Router, Route, Switch, Redirect, NavLink } from 'react-router-dom'
+
+import '@/assets/stylus/reset.styl'
+import logo from '@/assets/images/logo.png'
+import './App.styl'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="app">
+          <header className="app-header">
+            <img src={ logo } className="app-logo" alt="logo"/>
+            <h1 className="app-title">QQ Music</h1>
+          </header>
+          <div className="music-tab">
+            <div className="tab-item selected">
+              <NavLink to="/recommend" className="nav-link">
+                <span>推荐</span>
+              </NavLink>
+            </div>
+            <div className="tab-item">
+              <NavLink to="/ranking" className="nav-link">
+                <span>排行榜</span>
+              </NavLink>
+            </div>
+            <div className="tab-item">
+              <NavLink to="/search" className="nav-link">
+                <span>搜索</span>
+              </NavLink>
+            </div>
+          </div>
+          <div className="music-view">
+            <Switch>
+              <Route path="/recommend" component={ Recommend }/>
+              <Route path="/ranking" component={ Ranking }/>
+              <Route path="/search" component={ Search }/>
+              {/* 默认路由 Recommend */}
+              <Redirect from="/" to="recommend"/>
+              <Route component={ Recommend }/>
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App
