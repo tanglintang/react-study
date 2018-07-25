94b63c4279e49e675819c733cf89baa22d9e5494
tanglin <2196184435@qq.com>
2018-07-25 18:46:58 +0800

    模型层&&scroll组件

--- react-music/README.md
+++ react-music/README.md
@@ -36,3 +36,48 @@ function buildURL (url, data) {
   return url
 }
 ```
+
+## 安按照时间排序
+```js
+albumList.sort((a, b) => {
+    return new Date(b.public_time).getTime() - new Date(a.public_time).getTime()
+})
+```
+- nnew Date(b.public_time).getTime() 转换为时间戳
+- sort()
+> 比较两个值，然后返回一个用于说明这两个值的相对顺序的数字
+
+    若 a 小于 b，在排序后的数组中 a 应该出现在 b 之前，则返回一个小于 0 的值。
+    若 a 等于 b，则返回 0。
+    若 a 大于 b，则返回一个大于 0 的值。
+
+## 前端模型层
+`src/model`
+
+```js
+// 通过 model 模型层 过滤数据 提供一个对象
+const album = AlbumModel.createAlbumByList(item)
+```
+```js
+// 模型层 做数据处理 
+// 字段拼接。。
+export function createAlbumByList (data) {
+    return new Album(
+        data.album_id,
+        data.album_mid,
+        data.album_name,
+        `http://y.gtimg.cn/music/photo_new/T002R300x300M000${data.album_mid}.jpg?max_age=2592000`,
+        filterSinger(data.singers),
+        data.public_time
+    )
+}
+
+function filterSinger (singers) {
+    let singerArr = singers.map(singer => {
+        return singer.singer_name
+    })
+    return singerArr.join('/')
+}
+```
+
+## prop-types 参数检测
--- react-music/src/components/recommend/Recommend.js
+++ react-music/src/components/recommend/Recommend.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
-import { getCarousel } from '@/api/recommend'
+import { getCarousel, getNewAlbum } from '@/api/recommend'
+import { CODE_SUCCESS } from '../../api/config'
+import * as AlbumModel from '@/model/album'
 import Swiper from 'swiper'
+import Scroll from '@/common/scroll/Scroll'
 import 'swiper/dist/css/swiper.css'
 import './recommend.styl'
-import { CODE_SUCCESS } from '../../api/config';
 
 class Recommend extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      sliderList: []
+      sliderList: [],
+      newAlbums: [],
+      refreshScroll: false
     }
   }
 
@@ -33,27 +37,85 @@ class Recommend extends Component {
         }
       }
     })
+    getNewAlbum().then(res => {
+      if (res) {
+        if (res.code === CODE_SUCCESS) {
+          let albumList = res.albumlib.data.list
+          albumList.sort((a, b) => {
+            return new Date(b.public_time).getTime() - new Date(a.public_time).getTime()
+          })
+          // console.log(albumList)
+          this.setState({
+            newAlbums: albumList
+          }, () => {
+            this.setState({
+              refreshScroll: true
+            })
+          })
+        }
+      }
+    })
+  }
+
+  toLink(linkUrl) {
+    // 箭头函数 可以不用 bind(this)
+    // 实际调用的是 返回的箭头函数
+    // 而箭头函数 this 指向父级 => Recommend
+    return () => {
+      // console.log(this)
+      window.location.href = linkUrl
+    }
   }
 
   render() {
+
+    const albums = this.state.newAlbums.map(item => {
+      // console.log(item)
+      // 通过 model 模型层 过滤数据 提供一个对象
+      const album = AlbumModel.createAlbumByList(item)
+
+      return (
+        <div className="album-wrapper" key={album.mId}>
+          <div className="left">
+            <img src={album.img} alt={album.name} width="100%" height="100%" />
+          </div>
+          <div className="right">
+            <div className="album-name">{album.name}</div>
+            <div className="singer-name">{album.singer}</div>
+            <div className="public-time">{album.publicTime}</div>
+          </div>
+        </div>
+      )
+    })
+
     return (
       <div className="music-recommend">
-        <div className="slider-container">
-          <div className="swiper-wrapper">
-            {
-              this.state.sliderList.map(slider => {
-                return (
-                  <div className="swiper-slide" key={slider.id}>
-                    <a href="#" className="slider-nav">
-                      <img src={slider.picUrl} alt="推荐" width="100%" height="100%" />
-                    </a>
-                  </div>
-                )
-              })
-            }
+        <Scroll refresh={ this.state.refreshScroll }>
+          <div>
+            <div className="slider-container">
+              <div className="swiper-wrapper">
+                {
+                  this.state.sliderList.map(slider => {
+                    return (
+                      <div className="swiper-slide" key={slider.id}>
+                        <a href="#" className="slider-nav" onClick={this.toLink(slider.linkUrl)}>
+                          <img src={slider.picUrl} alt="推荐" width="100%" height="100%" />
+                        </a>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+              <div className="swiper-pagination"></div>
+            </div>
+            <div className="album-container">
+              <h1 className="title">最新专辑</h1>
+              <div className="album-list">
+                {albums}
+              </div>
+            </div>
           </div>
-          <div className="swiper-pagination"></div>
-        </div>
+        </Scroll>
       </div>
     )
   }
